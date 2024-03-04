d5e0c44e54e6c693e0fde7d31215a76ee4a8513a
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2024-03-04 17:40:38 -0500

    MVP Complete

--- frontend/index.js
+++ frontend/index.js
@@ -4,9 +4,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
+import { QuotesProvider } from './context/quotesContext'
+
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
-  <App />
-)
+	<QuotesProvider>
+		<App />
+	</QuotesProvider>
+);
